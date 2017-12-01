7230589e52f49fc56892b8421c1188ee53a52483
Sofikul <sofikul.projects@gmail.com>
2017-12-01 20:34:48 +0530

    changed app port

--- server/common/config/application-properties.js
+++ server/common/config/application-properties.js
@@ -2,7 +2,7 @@ module.exports = {
 	dbUrl : 'mongodb://taskcontroll:taskcontroll@ds125126.mlab.com:25126/taskcontroll',
 	serverPort : '8081',
 	oAuth : {
-		tokenSecret: '',
+		tokenSecret: '1234',
 		facebook : {
 			clientSecret :''		
 		}
